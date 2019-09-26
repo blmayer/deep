8c8941ede6c40d0e2a59083d447d0d294bddfaaf
blmayer <bleemayer@gmail.com>
2019-09-26 01:01:43 -0300

    Added some docstrings

--- R/neural-nets.R
+++ R/neural-nets.R
@@ -1,103 +1,29 @@
-mcCullochPitts <- setRefClass(
-    "McCullochPitts",
-    fields = list(ws = "numeric", bias = "numeric"),
-    methods = list(
-        initialize = function(ws, bias) {
-            ws <<- ws
-            bias <<- bias
-        },
-        output = function(inputs) {
-            sum(ws*inputs) + bias
-        },
-        train = function(ins, outs, epochs = 1, tax = .01, maxErr = 0) {
-            err <- vector("numeric", nrow(ins))
-            repeat {
-                for (i in 1:nrow(ins)) {
-                    err[i] <- outs[i] - output(ins[i,])
-                }
-                e <- mean(err)
-                ws <<- unlist(ws + tax*e*ins[i,], use.names = F)
-                bias <<- bias + e*tax
+#' deep: A Neural Networks Framework
+#'
+#' The deep package provides classes for layers, types of neurons and the
+#' neural network as a whole.
+#'
+#' @docType package
+#' @name deep
+NULL
 
-                epochs <- epochs - 1
-                if (epochs == 0 || abs(e) < maxErr) {
-                    break()
-                }
-            }
-        }
-    )
-)
 
-mcCullochPittsLayer <- setRefClass(
-    "McCullochPittsLayer",
-    fields = list(n = "numeric", dims = "vector", neurons = "vector"),
-    methods = list(
-        initialize = function(n, len) {
-            n <<- n
-            dims <<- c(n, len)
-            neurons <<- replicate(n,
-                                  mcCullochPitts$new(ws = runif(len), bias = 1))
-        },
-        output = function(inputs) {
-            lapply(neurons, function(x) x$output(inputs))
-        },
-        train = function(ins, outs, epochs = 1, tax = .01, maxErr = 0) {
-            for (i in 1:n) {
-                neurons[[i]]$train(ins, outs[,i], epochs, tax, maxErr)
-            }
-        }
-    )
-)
-
-perceptron <- setRefClass(
-    "Perceptron",
-    fields = list(ws = "numeric", bias = "numeric"),
-    methods = list(
-        initialize = function(ws, bias) {
-            ws <<- ws
-            bias <<- bias
-        },
-        output = function(inputs) {
-            as.integer(sum(ws*inputs) + bias > 0)
-        },
-        train = function(ins, outs, epochs = 1, tax = .01, maxErr = 0) {
-            err <- vector("numeric", nrow(ins))
-            repeat {
-                for (i in 1:nrow(ins)) {
-                    err[i] <- outs[i] - output(ins[i,])
-                }
-                e <- mean(err)
-                ws <<- unlist(ws + tax*e*ins[i,], use.names = F)
-                bias <<- bias + e*tax
-
-                epochs <- epochs - 1
-                if (epochs == 0 || abs(e) < maxErr) {
-                    break()
-                }
-            }
-        }
-    )
-)
-
-perceptronLayer <- setRefClass(
-    "PerceptronLayer",
-    fields = list(n = "numeric", dims = "vector", neurons = "vector"),
-    methods = list(
-        initialize = function(n, len = 1) {
-            n <<- n
-            dims <<- c(n, len)
-            neurons <<- replicate(n, perceptron(runif(len), 1))
-        },
-        output = function(inputs) {
-            sapply(neurons, function(x) x$output(inputs))
-        },
-        train = function(ins, outs, epochs = 1, tax = .1, maxErr = 0) {
-            lapply(neurons,
-                   function(x) x$train(ins, outs, epochs, tax, maxErr))
-        }
-    )
-)
-
+#' The main NeuralNetwork class, that holds the layers.
+#'
+#' @field eta The learning tax, representes the size of the weight adjustment
+#' between each epoch of training.
+#'
+#' @field layers This field is a list of the layers of the network, you can use
+#' subsetting to inspect them.
+#'
+#' @method initialize This run when you instantiate the class, either with
+#' \code{$new()} or \code{neuralNet()}, the intent is to set up the fields.
+#'
+#' @method compute Gives the output of passing data inside the network.
+#' @param input The actual data to be fed into the network, this input's
+#' dimentions vary with the first layer in your network.
+#' @return Vector of the outputs of the last layer in your network.
+#'
 neuralNet <- setRefClass(
     "NeuralNetwork",
     fields = list(eta = "numeric", layers = "vector"),
@@ -107,15 +33,15 @@ neuralNet <- setRefClass(
             layers <<- vector("list", ...length())
             for (i in 1:...length()) {
                 switch (class(...elt(i)),
-                    "PerceptronLayer" = {
-                        layers[[i]] <<- perceptronLayer(...elt(i)$n, input)
-                        input <- ...elt(i)$n
-                    },
-                    "McCullochPittsLayer" = {
-                        layers[[i]] <<- McCullochPittsLayer(...elt(i)$n, input)
-                        input <- ...elt(i)$n
-                    },
-                    stop("argument in ... is not a layer!")
+                        "PerceptronLayer" = {
+                            layers[[i]] <<- perceptronLayer(...elt(i)$n, input)
+                            input <- ...elt(i)$n
+                        },
+                        "McCullochPittsLayer" = {
+                            layers[[i]] <<- mcCullochPittsLayer(...elt(i)$n, input)
+                            input <- ...elt(i)$n
+                        },
+                        stop("argument in ... is not a layer!")
                 )
             }
         },
